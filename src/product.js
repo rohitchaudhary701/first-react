a4b93acedce8bb8a6b17655019b644ee2eb8d232
rohitchaudhary701 <rohitchaudhary701@gmail.com>
2019-01-17 16:13:55 +0530

    My React Program

---
+++ src/product.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import products from './product'
+
+function products(props){
+
+}
+
+export default products;
---
+++ src/schoolProducts.js
@@ -0,0 +1,40 @@
+const products = [
+	{
+		id: "1",
+		name: "Pencil",
+		price: 1,
+		description: "Perfect for those"
+	},
+	{
+		id: "2",
+		name: "Housing",
+		price: 10,
+		description: "Housing provided for out-of-state"
+	},
+	{
+		id: "3",
+		name: "Computer",
+		price: 25000,
+		description: "Don't have a computer? no problem"
+	},
+	{
+		id: "4",
+		name: "Coffee",
+		price: 360,
+		description: "Wake up!"
+	},
+	{
+		id: "5",
+		name: "Snacks",
+		price: 1200,
+		description: "Free snacks!"
+	},
+	{
+		id: "6",
+		name: "Rubber",
+		price: 18,
+		description: "Rubber Duckies"
+	},
+]
+
+export default products;
